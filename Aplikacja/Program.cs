26b5e744e1693dfc6a964e6c8a22219e2ba8218b
Pawel Mikolajczyk <bratpawel00@gmail.com>
2023-09-16 20:12:01 +0200

    Dzien 4 Poprawione

--- Aplikacja/Program.cs
+++ Aplikacja/Program.cs
@@ -1,10 +1,10 @@
-﻿string name = Ewa;
-string płeć = kobieta;
-int wiek = 33;
+﻿string name = "Ewa";
+string plec = "kobieta";
+var wiek = 33;
 
 
 
-if (name == Ewa && wiek == 33)
+if (name == "Ewa" && wiek == 33) 
 {
     Console.WriteLine("Nazywasz się Ewa i masz 33 lata");
 }
