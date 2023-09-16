a7a9a49ba59a38665047371f75ffa674d768c94c
Pawel Mikolajczyk <bratpawel00@gmail.com>
2023-09-16 16:39:42 +0200

    Dzień 4

--- Aplikacja/Program.cs
+++ Aplikacja/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = Ewa;
+string płeć = kobieta;
+int wiek = 33;
+
+
+
+if (name == Ewa && wiek == 33)
+{
+    Console.WriteLine("Nazywasz się Ewa i masz 33 lata");
+}
+
+else 
+{
+    Console.WriteLine("Nie nazywasz się Ewa i nie masz 33 lat");
+}
+ 
