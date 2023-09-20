26a7d8b57bd622bb4d4192621fe8575a869eb645
Pawel Mikolajczyk <bratpawel00@gmail.com>
2023-09-20 22:22:35 +0200

    Dzien 5

--- Aplikacja/Program.cs
+++ Aplikacja/Program.cs
@@ -1,23 +1,64 @@
-﻿
+﻿int number = 2187;
+string numberToString =  number.ToString();
+char[] letters = numberToString.ToCharArray();
 
-    
-        string name = "Paweł";
-        string gender = "kobieta";
-        int age = 9;
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+
+foreach (char letter in letters)
+{
+    if(letter == '0')
+    {
+        counter0++;
+    }
+    else if(letter == '1')
+    {
+        counter1++;
+    }
+    else if(letter == 2)
+    {
+        counter2++;
+    }
+    else if(letter == 3)
+    {
+        counter3++;
+    }
+    else if (letter == 4)
+    {
+        counter4++; 
+    }
+    else if(letter == 5)
+    {
+        counter5++; 
+    }
+    else if (letter == 6)
+    {
+        counter6++; 
+    }
+    else if(letter == 7)
+    {
+        counter7++; 
+    }
+    else if (letter == 8)
+    {
+        counter8++;
+    }
+    else if (letter == 9)
+    {
+        counter9++; 
+    }
+
+    Console.WriteLine(letter);
+}
 
 
 
-        if (name == "Paweł" && age >= 18)
-        {
-            Console.WriteLine("Możesz kupić piwo");
-        }
-
-        else if (gender != "kobieta" || age <= 18)
-        {
-            Console.WriteLine("Niepełnoletni mężczyzna");
-        }
-        else
-        {
-            Console.WriteLine("nie możesz kupić piwa");
-        }
-    
